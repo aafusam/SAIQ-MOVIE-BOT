e709e06d14e76dd517ffd6f5aaa2a75add28097d
aafusam <62737721+aafusam@users.noreply.github.com>
2022-11-15 17:06:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,8 +47,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://t.me/saiq_bots'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/saiq_bots')
+            InlineKeyboardButton('⚡ ꜱᴀɪQ ᴍᴏᴠɪᴇꜱ ⚡', url='https://t.me/saiqMmovies'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇꜱ 🤖', url='https://t.me/saiq_bots')
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
@@ -93,8 +93,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://t.me/saiq_bots'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/saiq_bots')
+            InlineKeyboardButton('⚡ ꜱᴀɪQ ᴍᴏᴠɪᴇꜱ ⚡', url='https://t.me/saiqmovies'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇꜱ🤖', url='https://t.me/saiq_bots')
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
